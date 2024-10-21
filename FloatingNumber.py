c0260c85011a14a6b94e0ff5b2550d8896313340
Aritiku <arielleigan@gmail.com>
2024-10-21 10:13:52 -0700

    Add files via upload

---
+++ FloatingNumber.py
@@ -0,0 +1,26 @@
+def main():
+
+    #Floating number calc
+    x = 7.7
+    y = 1.2
+    a = x + y
+    print("Sum:", a)
+    print("Data type of sum:", type(a))
+
+    #Integer calc
+    l = 21
+    m = 5
+    b = l - m
+    print("Difference:", b)
+    print("Data type of difference:", type(b))
+
+    #Floating number and integer calc
+    z = 6.8
+    n = 3
+    c = z * n
+    print("Product:", c)
+    print("Data type of product:", type(c))
+
+
+if __name__ == '__main__': 
+    main()
