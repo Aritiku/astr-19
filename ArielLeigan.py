df9a5df435e76fd47f07e2afc28278a4d5332864
Aritiku <arielleigan@gmail.com>
2024-10-21 09:35:50 -0700

    Add files via upload

---
+++ ArielLeigan.py
@@ -0,0 +1,12 @@
+#Tells computer to print Ariel Leigan
+def PrintArielLeigan():
+	print("Ariel Leigan")
+	print("My favorite movie is Princess Mononoke.\nMy favorite food is salmon sashimi.")
+
+#Defines main function for program
+def main():
+	PrintArielLeigan()
+
+#When program is ran this is executed first
+if __name__ == '__main__':
+	main()
